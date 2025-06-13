a90b465c7d4d60c5dd78de159ca143f320f35002
Roman A <121314722+GameRoMan@users.noreply.github.com>
2025-06-13 20:10:59 +0100

    create isOneOf

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export function isOneOf<TValue, const TAllowed extends TValue>(
+  value: TValue,
+  allowed: ReadonlyArray<TAllowed>
+): value is TAllowed {
+  return allowed.includes(value as TAllowed);
+}
